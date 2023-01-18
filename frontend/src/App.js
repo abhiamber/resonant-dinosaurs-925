ebebf1b1edd54111b00e7c81097a1750afa02148
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2023-01-18 22:24:47 +0530

    final done conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import AllRoutes from "./Pages/Allroutes";
 import SearchContextProvider from "./Utilis/Context/SearchContext";
 import { CartContextProvider } from "./Utilis/Context/CartContext";
-import Navbar from './Components/navbar/Navbar';
-import Header from "./Components/Header";
-import SearchBox from "./Components/SearchBox";
+import Navbar from './components/navbar/Navbar';
+import Header from "./components/Header";
+import SearchBox from "./components/SearchBox";
 function App() {
   return (
     <div>
