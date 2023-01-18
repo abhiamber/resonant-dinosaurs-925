40737c916505fce8df2067b79349636cd87c1875
ashish bajaj <abajaj655@gmail.com>
2023-01-18 20:59:21 +0530

    day-2 work done by Ashish

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import AsCatSlider from "../components/AsCatSlider";
+import AsImgGallery from "../components/AsImgGallery";
+
+export default function Home() {
+  return (
+    <>
+      <AsImgGallery />
+      <AsCatSlider />
+    </>
+  );
+}
