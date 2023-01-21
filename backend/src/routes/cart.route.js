be4205d6530ec557755b9aa641b7abae89f4e3db
gprasadbhat948231 <gprasadbhat948231@gmail.com>
2023-01-21 20:51:49 +0530

    added cart route

--- backend/src/routes/cart.route.js
+++ backend/src/routes/cart.route.js
@@ -1,12 +1,25 @@
 const express = require("express")
 const { authuser } = require('../middleware/cart.middleware');
-const { CartModel } = require("../models/cart.model")
+const { CartModel } = require("../models/cart.model");
+const UserModel = require("../models/user.model")
 const cartRouter = express.Router()
-
-cartRouter.use(authuser);
-cartRouter.get("/", async (req, res) => {
+cartRouter.get("/cartuser", async (req, res) => {
+    const email = req.headers.email;
     try {
-        const data = await CartModel.find()
+        const userID = await UserModel.findOne({ "email": email })
+        res.send(userID)
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+cartRouter.use(authuser);
+
+cartRouter.get("/", async (req, res) => {
+    const userid = req.headers.user;
+    console.log(userid)
+    try {
+        const data = await CartModel.find({ "userID": userid })
         res.send(data)
     }
     catch (err) {
@@ -30,7 +43,7 @@ cartRouter.post("/additemtocart", async (req, res) => {
 cartRouter.delete("/delete/:id", async (req, res) => {
     const id = req.params.id
     try {
-        await CartModel.findByIDAndDelete({ "_id": id })
+        await CartModel.findByIdAndDelete({ "_id": id })
         res.send("Product has been removed successfully")
     }
     catch (err) {
