1991711e42aae3b3cda64c79dfb56c79238ff085
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2023-01-23 18:49:54 +0530

    aded

--- backend/src/routes/prod.route.js
+++ backend/src/routes/prod.route.js
@@ -4,32 +4,30 @@ const app = express.Router();
 const ProdModel = require("../models/product.model");
 
 app.get("/", async (req, res) => {
-  const { query } = req.query;
+  const { query, page = 1, limit = 20 } = req.query;
   //   console.log(query, "t");
 
   try {
     if (query) {
       let data = await ProdModel.find({
         product_type: { $regex: query, $options: "i" },
-      });
+      }).skip((page - 1) * limit)
+        .limit(limit);
       if (data.length > 0) {
-        // console.log(query, "t", data);
-
         return res.send({ messg: data, state: "OK" });
       } else {
         let data = await ProdModel.find({
           category: { $regex: query, $options: "i" },
-        });
-        // console.log(query, "z", data);
-
+        }).skip((page - 1) * limit)
+          .limit(limit);
         return res.send({ messg: data, state: "OK" });
       }
     }
     const data = await ProdModel.find();
-    return res.send({ messg: data, state: "OK" });
+    return res.send({ messg: data, state: "OK" }).skip(page - 1)
+      .limit(limit);;
   } catch (e) {
     return res.send({ messg: e.message, state: "NOT" });
-    // console.log(err);
   }
 });
 
